60fd47299f3f4a5d712310690efaeb599879077c
Thomas Brasser <thomas@brasser.family>
2023-04-05 19:50:29 +0200

    fixes for ha 202304

---
+++ www/persistentsettings.js
@@ -0,0 +1,34 @@
+// Persistent settings across multiple devices / sessions
+// ------------------------------------------------------
+(function () {
+  // Visit /profile?edit=1 to change your settings, then update them here.
+  const settings = {
+    dockedSidebar: '"always_hidden"',
+    enableShortcuts: "false",
+    suspendWhenHidden: "false",
+    vibrate: "true",
+  };
+  let settingsUpdated = false;
+  const currentSettings = {};
+  Object.keys(settings).forEach((key) => {
+    currentSettings[key] = localStorage.getItem(key);
+    if (currentSettings[key] !== settings[key]) {
+      localStorage.setItem(key, settings[key]);
+      settingsUpdated = true;
+    }
+  });
+  const urlSearchParams = new URLSearchParams(location.search);
+  if (!settingsUpdated) {
+    console.log("Settings are up to date.");
+    return;
+  }
+  if (urlSearchParams.get("edit") === "1") {
+    console.warn(
+      "Settings updated:\n",
+      JSON.stringify(currentSettings, null, 2)
+    );
+  } else {
+    console.warn("Settings updated, reloading page...");
+    location.reload();
+  }
+})();
--- www/style.js
+++ www/style.js
@@ -4,7 +4,7 @@ function resizeGridItem(item){
 }
 
 function resizeAllGridItems(){
-  var grid = document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view > hui-panel-view").shadowRoot.querySelector("mod-card").shadowRoot.querySelector("ha-card > state-switch").shadowRoot.querySelector("#root > div.visible > hui-vertical-stack-card").shadowRoot.querySelector("#root > layout-card").shadowRoot.querySelector("grid-layout").shadowRoot.querySelector("#root");
+  var grid = document.querySelector("body > home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.querySelector("#view > hui-view > hui-panel-view").shadowRoot.querySelector("mod-card").shadowRoot.querySelector("ha-card > state-switch").shadowRoot.querySelector("#root > div.visible > hui-vertical-stack-card").shadowRoot.querySelector("#root > layout-card").shadowRoot.querySelector("grid-layout").shadowRoot.querySelector("#root");
   var allItems = grid.children;
   for(var x=0;x<allItems.length;x++){
     resizeGridItem(allItems[x]);
